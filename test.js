88dc6a342cdf3e644da7475b69aee30c5e6f26d9
Yuriy Nemtsov <nemtsov@gmail.com>
2014-12-18 02:38:49 -0500

    tests: add more cases

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 var assert = require('assert'),
   reverse = require('./');
 
-assert.equal(reverse('abcd'), 'dcba');
-assert.equal(reverse('abc'), 'cba');
-assert.equal(reverse('ab𝌄de'), 'ed𝌄ba');
-assert.equal(reverse('abc𝌄e'), 'e𝌄cba');
+assert.equal(reverse(''), '');
+assert.equal(reverse('a'), 'a');
+assert.equal(reverse('𝌄'), '𝌄');
 assert.equal(reverse('𝌄d'), 'd𝌄');
 assert.equal(reverse('a𝌄'), '𝌄a');
+assert.equal(reverse('abc'), 'cba');
+assert.equal(reverse('abcd'), 'dcba');
+assert.equal(reverse('ab𝌄de'), 'ed𝌄ba');
+assert.equal(reverse('abc𝌄e'), 'e𝌄cba');
 
 console.log('ok');
